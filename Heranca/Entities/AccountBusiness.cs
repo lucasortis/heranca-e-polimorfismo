04e7496c4080e1166ddec7d730f1a490b3b1de16
Lucas Ortis <lucasortis01@gmail.com>
2021-09-06 21:13:12 -0300

    aplicação para treinar os conceitos de herança

---
+++ Heranca/Entities/AccountBusiness.cs
@@ -0,0 +1,28 @@
+﻿namespace Heranca.Entities
+{
+    class AccountBusiness : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public AccountBusiness()
+        {
+
+        }
+
+        public AccountBusiness(double loanLimit, int number, string holder, double balance) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountBusiness acc1 = new AccountBusiness(200, 3401, "Lucas Ortis", 2000);
+
+            Console.WriteLine(acc1.Balance);
+
+            // acc1.Balance = 20;
+        }
+    }
+}
